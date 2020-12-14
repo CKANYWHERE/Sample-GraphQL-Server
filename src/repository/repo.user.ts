70e19e7c1400f53b354cd28d75c016068ec25bbe
Min-Chang-Kyeoung <janu723@naver.com>
2020-12-14 14:35:34 +0900

    create User Repository

---
+++ src/repository/repo.user.ts
@@ -0,0 +1,24 @@
+import { EntityRepository, Repository } from "typeorm";
+import { User } from '../model/model.user';
+import { UserInput } from "../resolvers/InputType/user.inputType";
+
+
+export class UserRepository extends Repository<User>{
+
+    /*
+        모든 유저 찾는 쿼리
+    */
+    async findAllUser(){
+        return await this.createQueryBuilder("t_User")
+                    .getMany();
+    }
+
+    /*
+        User insert 하는 코드
+        @params UserInput
+    */
+    async insertUser(user: UserInput){
+        await this.insert(user)
+    }
+
+}
--- src/resolvers/resolver.user.ts
+++ src/resolvers/resolver.user.ts
@@ -1,15 +1,25 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import { getManager } from "typeorm";
+import {  getConnection, getConnectionManager, getCustomRepository, getManager } from "typeorm";
 import { User } from '../model/model.user';
+import { UserRepository } from '../repository/repo.user';
 import { UserInput } from './InputType/user.inputType';
+
+// const manager = getConnectionManager().get("gamepartner");
+// const repo = manager.getCustomRepository(UserRepository);
 
 @Resolver()
 export class UserResolver{
 
     @Mutation(() => Boolean)
-    
-    async createUser(@Arg("user") user:UserInput){
-        return true
+    async createUser(@Arg("user", () => UserInput) user:UserInput){
+        try{
+            await User.insert(user);
+            return true
+        }
+        catch(err){
+            console.log(err)
+            return false
+        }
     }
 
     @Query(() => [User])
